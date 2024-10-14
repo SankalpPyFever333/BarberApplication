2b2df03444205fde99005c4d24dab02679d88805
sankalp <sankalppandey696@gmail.com>
2024-10-15 00:18:52 +0530

    addd fonts and images and building the onboarding page

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -10,11 +10,38 @@ class Onboarding extends StatefulWidget {
 class _OnboardingState extends State<Onboarding> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
+      backgroundColor: const Color(0xFF2b1615),
       body: Container(
-        child: const Column(
+        // width: size.width,
+        // height: size.height,
+        // width: 400,
+        // height: 400,
+        // margin:const EdgeInsets.only(top: 120 , left: 120),
+        child: Column(
           children: [
-            
+            Image.asset(
+              "images/barber.png",
+              fit: BoxFit.contain,
+            ),
+            Container(
+              padding:const EdgeInsets.symmetric(horizontal: 20 , vertical: 10),
+              decoration: BoxDecoration(
+                color: const  Color(0xFFdf711a),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Text(
+                "Get a Stylish hair cut",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    fontFamily: 'Signi'
+                    ),
+              ),
+            ),
           ],
         ),
       ),
