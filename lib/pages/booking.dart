52dd7db1f47ae649d86458175ce4c5192be9918d
sankalp <sankalppandey696@gmail.com>
2024-10-16 00:00:28 +0530

    user can select date and time for booking service now

--- lib/pages/booking.dart
+++ lib/pages/booking.dart
@@ -10,6 +10,30 @@ class Booking extends StatefulWidget {
 
 class _BookingState extends State<Booking> {
   DateTime _selectedDateTime = DateTime.now();
+  TimeOfDay _currentTime = TimeOfDay.now();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context, firstDate: DateTime(2024), lastDate: DateTime(2025));
+
+    if (picked != null && picked != _selectedDateTime) {
+      setState(() {
+        _selectedDateTime = picked;
+      });
+    }
+  }
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? timePicked =
+        await showTimePicker(context: context, initialTime: _currentTime);
+
+    if (timePicked != null && timePicked != _currentTime) {
+      setState(() {
+        _currentTime = timePicked;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -79,14 +103,101 @@ class _BookingState extends State<Booking> {
                   SizedBox(
                     height: 10,
                   ),
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    GestureDetector(
+                      onTap: () {
+                        _selectDate(context);
+                      },
+                      child: Icon(
+                        Icons.calendar_month,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      "${_selectedDateTime.day}/${_selectedDateTime.month}/${_selectedDateTime.year}",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ]),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: Color(0xFFb4817e),
+                  borderRadius: BorderRadius.circular(20)),
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.only(top: 10, bottom: 10),
+              margin: EdgeInsets.only(right: 10),
+              child: Column(
+                children: [
                   Text(
-                    "${_selectedDateTime.day}/${_selectedDateTime.month}/${_selectedDateTime.year}",
+                    "Set Time",
                     style: TextStyle(
                         color: Colors.black,
                         fontSize: 20,
                         fontWeight: FontWeight.w500),
                   ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    GestureDetector(
+                      onTap: () {
+                        _selectTime(context);
+                      },
+                      child: Icon(
+                        Icons.alarm,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      _currentTime.format(context),
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ]),
                 ],
+              ),
+            ),
+            SizedBox(height: 20.0),
+
+            GestureDetector(
+              onTap: (){
+
+              },
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFdf711a),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: const Text(
+                    "BOOK NOW",
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
               ),
             ),
           ],
