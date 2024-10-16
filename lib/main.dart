3f012cec1949035b6295c3e92af9045b19fb6f0f
sankalp <sankalppandey696@gmail.com>
2024-10-16 23:39:09 +0530

    configured firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,13 @@ import 'package:barber_app/pages/login.dart';
 import 'package:barber_app/pages/onboarding.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import "firebase_options.dart";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -38,7 +41,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Login(),
+      home: const Home(),
     );
   }
 }
