9832c3b2d2ec8063ed8fda3553675683261a22bc
sankalp <sankalppandey696@gmail.com>
2024-10-17 00:59:10 +0530

    added method for saving user credentials

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,4 +1,6 @@
+import "package:barber_app/pages/home.dart";
 import "package:barber_app/pages/login.dart";
+import "package:firebase_auth/firebase_auth.dart";
 import "package:flutter/material.dart";
 import "package:flutter/gestures.dart";
 
@@ -10,13 +12,44 @@ class Signup extends StatefulWidget {
 }
 
 class _SignupState extends State<Signup> {
-
+  String name = '', email = '', password = '';
   TextEditingController nameController = TextEditingController();
-  TextEditingController emailController =  TextEditingController();
+  TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
+  final _formkey = GlobalKey<FormState>();
 
+// for checking, we need validator, that is available in TextFormField, we use this instead of TextField.
 
+  registration() async {
+    try {
+      if (email.isEmpty && password.isEmpty && name.isEmpty) {
+        UserCredential userCredential = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(email: email, password: password);
+      }
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(
+        "Registered successfully",
+        style: TextStyle(fontSize: 20, fontFamily: "Signi"),
+      )));
+
+      Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text(
+          "password is oo weak",
+          style: TextStyle(fontSize: 20, fontFamily: "Signi"),
+        )));
+      } else if (e.code == "email-already-in-use") {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text(
+          "Account already exists",
+          style: TextStyle(fontSize: 20, fontFamily: "Signi"),
+        )));
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,118 +86,149 @@ class _SignupState extends State<Signup> {
                     topLeft: Radius.circular(20),
                     topRight: Radius.circular(20)),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Name",
-                    style: TextStyle(
-                        color: Color(0xFFB91635),
-                        fontSize: 23,
-                        fontWeight: FontWeight.w500,
-                        fontFamily: "Signi"),
-                  ),
-                  TextField(
-                    controller: nameController,
-                    decoration: InputDecoration(
-                      hintText: "Name",
-                      labelText: "Enter Name",
-                      prefixIcon: Icon(Icons.abc_outlined),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Text(
-                    "Email",
-                    style: TextStyle(
-                        color: Color(0xFFB91635),
-                        fontSize: 23,
-                        fontWeight: FontWeight.w500,
-                        fontFamily: "Signi"),
-                  ),
-                  TextField(
-                    controller: emailController,
-                    decoration: InputDecoration(
-                      hintText: "Email",
-                      labelText: "Enter Email",
-                      prefixIcon: Icon(Icons.mail_outline),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  TextField(
-                    controller: passwordController,
-                    decoration: InputDecoration(
-                      hintText: "Password",
-                      labelText: "Enter Password",
-                      prefixIcon: Icon(Icons.password_outlined),
-                    ),
-                    obscureText: true,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  SizedBox(
-                    height: 50,
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(colors: [
-                        Color(0xFFB91635),
-                        Color(0xFF621d3c),
-                        Color(0xFF311917)
-                      ]),
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: Text(
-                      "SIGN UP",
+              child: Form(
+                key: _formkey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Name",
                       style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 23,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: "Signi",
-                      ),
-                      textAlign: TextAlign.center,
+                          color: Color(0xFFB91635),
+                          fontSize: 23,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: "Signi"),
                     ),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      RichText(
-                        text: TextSpan(children: [
-                          TextSpan(
-                            text: "Already have an account? ",
-                            style: TextStyle(
-                              color: Color(0xFF311917),
-                              fontSize: 17,
-                              fontFamily: "Signi",
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          TextSpan(
-                              text: "log in",
-                              style: TextStyle(
-                                color: Colors.blue,
-                                fontSize: 17,
-                                fontWeight: FontWeight.w600,
-                                fontFamily: "Signi",
-                              ),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  Navigator.pop(context);
-                                }),
-                        ]),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'please enter name';
+                        }
+                        return null;
+                      },
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        hintText: "Name",
+                        labelText: "Enter Name",
+                        prefixIcon: Icon(Icons.abc_outlined),
                       ),
-                    ],
-                  )
-                ],
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Text(
+                      "Email",
+                      style: TextStyle(
+                          color: Color(0xFFB91635),
+                          fontSize: 23,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: "Signi"),
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'please enter email';
+                        }
+                        return null;
+                      },
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        hintText: "Email",
+                        labelText: "Enter Email",
+                        prefixIcon: Icon(Icons.mail_outline),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'please enter password';
+                        }
+                        return null;
+                      },
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        labelText: "Enter Password",
+                        prefixIcon: Icon(Icons.password_outlined),
+                      ),
+                      obscureText: true,
+                    ),
+                    SizedBox(
+                      height: 60,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        if (_formkey.currentState!.validate()) {
+                          setState(() {
+                            email = emailController.text;
+                            name = nameController.text;
+                            password = passwordController.text;
+                          });
+                        }
+
+                        registration();
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(colors: [
+                            Color(0xFFB91635),
+                            Color(0xFF621d3c),
+                            Color(0xFF311917)
+                          ]),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Text(
+                          "SIGN UP",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 23,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "Signi",
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        RichText(
+                          text: TextSpan(children: [
+                            TextSpan(
+                              text: "Already have an account? ",
+                              style: TextStyle(
+                                color: Color(0xFF311917),
+                                fontSize: 17,
+                                fontFamily: "Signi",
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            TextSpan(
+                                text: "log in",
+                                style: TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w600,
+                                  fontFamily: "Signi",
+                                ),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    Navigator.pop(context);
+                                  }),
+                          ]),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ],
