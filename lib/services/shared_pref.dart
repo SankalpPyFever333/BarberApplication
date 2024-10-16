4af28eb151c06c9cc1995676b5162a9e88045fa3
sankalp <sankalppandey696@gmail.com>
2024-10-17 03:35:44 +0530

    creating shared_preference

---
+++ lib/services/shared_pref.dart
@@ -0,0 +1,8 @@
+class SharedPreferenceHelper{
+
+  static String userIdKey = "USERKEY";
+  static String userNameKey = "USERNAMEKEY";
+  static String userEmailKey = "USEREMAILKEY";
+  static String userImageKey = "USERIMAGEKEY";
+
+}
